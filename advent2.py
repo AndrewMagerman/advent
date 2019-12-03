1e98ac2f9bea8e1e180ec57a61b93c6233d477a3
Andrew Magerman <andrew.magerman@magerman.com>
2019-12-03 23:36:38 +0100

    refactor code

--- advent2.py
+++ advent2.py
@@ -31,7 +31,7 @@ class TuringMachine:
         return not self.operation() == 99
 
     def __str__(self):
-        return f'|{self.state} , {self.current_read_position}'
+        return f'{self.state} , {self.current_read_position}'
 
 
 def turing(state: List) -> List:
@@ -44,7 +44,16 @@ def turing(state: List) -> List:
 
 
 if __name__ == '__main__':
-    print(turing([1, 0, 0, 0, 99]))
-    # print(len(all_module_masses))
-    # print(fuel_for_trip())
-    # print(fuel_for_trip_with_fuel_eigenbedarf())
+
+    puzzle_state = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 6, 23, 2, 6, 23, 27, 2, 27, 9,
+                    31, 1, 5, 31, 35, 1, 35, 10, 39, 2, 39, 9, 43, 1, 5, 43, 47, 2, 47, 10, 51, 1, 51, 6, 55, 1, 5, 55,
+                    59, 2, 6, 59, 63, 2, 63, 6, 67, 1, 5, 67, 71, 1, 71, 9, 75, 2, 75, 10, 79, 1, 79, 5, 83, 1, 10, 83,
+                    87, 1, 5, 87, 91, 2, 13, 91, 95, 1, 95, 10, 99, 2, 99, 13, 103, 1, 103, 5, 107, 1, 107, 13, 111, 2,
+                    111, 9, 115, 1, 6, 115, 119, 2, 119, 6, 123, 1, 123, 6, 127, 1, 127, 9, 131, 1, 6, 131, 135, 1, 135,
+                    2, 139, 1, 139, 10, 0, 99, 2, 0, 14, 0]
+
+    puzzle_state[1] = 12
+    puzzle_state[2] = 2
+
+    print(puzzle_state)
+    print(turing(puzzle_state))
