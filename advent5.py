e491570670886592b4f6e642712efc8dd0462592
Andrew Magerman <andrew.magerman@magerman.com>
2019-12-05 10:39:11 +0100

    wip

--- advent5.py
+++ advent5.py
@@ -1,14 +1,37 @@
 from typing import List
 
+from collections import namedtuple
+
+instruction = namedtuple('instruction', 'opcode parametercount function')
+
 
 class TuringMachine:
 
-    def __init__(self, state, read_position: int):
+    def __init__(self, state, read_position: int = 0):
         self.state = state
         self.current_read_position = read_position
+        self.machine_input = None
 
     def operation(self):
         return self.state[self.current_read_position]
+
+    # def parameters_current_instruction(self) -> List:
+    #     ins = w[self.operation()]
+    #     listie = self.state[self.operation(): ins.parametercount + 1]
+    #     listie.reverse()
+    #
+    #     return listie
+
+    # @classmethod
+    def adding(self, listie):
+        stack = self.parameters_current_instruction()
+
+        pointer_value_1 = listie[0]
+        pointer_value_2 = listie[1]
+        pointer_ouput = listie[2]
+
+        self.state[self.state[pointer_ouput]] = self.state[self.state[pointer_value_1]] + self.state[
+            self.state[pointer_value_2]]
 
     def value(self, delta_to_read_position: int):
         position_of_value = self.state[self.current_read_position + delta_to_read_position]
@@ -22,44 +45,41 @@ class TuringMachine:
              2: lambda x, y: x * y,
              }
 
-        func = t[self.operation()]
-        self.state[self.output_position()] = func(self.value(1), self.value(2))
+        w = {1: instruction(opcode=1,
+                            parametercount=3,
+                            function=self.adding,
+                            ),
+             2: instruction(opcode=2,
+                            parametercount=3,
+                            function=self.adding,
+                            ),
 
-        self.current_read_position += 4
+             }
+
+        i = w[self.operation()]
+        listie = self.state[self.operation(): i.parametercount + 1]
+        i.function(listie)
+
+        # parameters = self.state[self.operation(): i.parametercount]
+        #
+        # func = t[self.operation()]
+        # self.state[self.output_position()] = func(self.value(1), self.value(2))
+
+        self.current_read_position += (i.parametercount + 1)
 
     def continue_ticking(self) -> bool:
         return not self.operation() == 99
 
+    def run(self, machine_input=None):
+        self.machine_input = machine_input
+
+        while self.continue_ticking():
+            self.tick()
+
+        return 19
+
     def __str__(self):
         return f'{self.state} , {self.current_read_position}'
-
-
-def turing(state: List) -> List:
-    a = TuringMachine(state=state, read_position=0)
-
-    while a.continue_ticking():
-        a.tick()
-
-    return a.state
-
-
-def computer(noun: int, verb: int) -> int:
-    puzzle_state = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 6, 23, 2, 6, 23, 27, 2, 27, 9,
-                    31, 1, 5, 31, 35, 1, 35, 10, 39, 2, 39, 9, 43, 1, 5, 43, 47, 2, 47, 10, 51, 1, 51, 6, 55, 1, 5, 55,
-                    59, 2, 6, 59, 63, 2, 63, 6, 67, 1, 5, 67, 71, 1, 71, 9, 75, 2, 75, 10, 79, 1, 79, 5, 83, 1, 10, 83,
-                    87, 1, 5, 87, 91, 2, 13, 91, 95, 1, 95, 10, 99, 2, 99, 13, 103, 1, 103, 5, 107, 1, 107, 13, 111, 2,
-                    111, 9, 115, 1, 6, 115, 119, 2, 119, 6, 123, 1, 123, 6, 127, 1, 127, 9, 131, 1, 6, 131, 135, 1, 135,
-                    2, 139, 1, 139, 10, 0, 99, 2, 0, 14, 0]
-    puzzle_state[1] = noun
-    puzzle_state[2] = verb
-    return turing(puzzle_state)[0]
-
-
-def task2(desired_output_puzzle_function: int) -> int:
-    for x in range(100):
-        for y in range(100):
-            if computer(noun=x, verb=y) == desired_output_puzzle_function:
-                return 100 * x + y
 
 
 if __name__ == '__main__':
@@ -96,5 +116,3 @@ if __name__ == '__main__':
                     102, 2, 223, 223, 1006, 224, 629, 1001, 223, 1, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1005,
                     224, 644, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 659, 101, 1, 223, 223,
                     107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226]
-
-    print(task2(desired_output_puzzle_function=19690720))
---
+++ test_advent5.py
@@ -0,0 +1,50 @@
+from advent5 import TuringMachine
+
+
+def turing(state):
+    e = TuringMachine(state=state)
+    e.run()
+    return e.state
+
+
+def t2(state, input):
+    e = TuringMachine(state=state)
+    return e.run(machine_input=input)
+
+
+def test_turing_machine_add():
+    e = TuringMachine(state=[1, 0, 0, 0, 99])
+    e.run()
+    assert e.state == [2, 0, 0, 0, 99]
+
+
+def test_turing_machine_multiply():
+    e = TuringMachine(state=[1, 0, 0, 0, 99])
+    e.run()
+    assert e.state == [2, 0, 0, 0, 99]
+
+    assert turing([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+    assert turing([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
+
+
+def test_turing_machine_mystery():
+    assert turing([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+
+def test_input():
+    assert t2([3, 0, 4, 0, 99], 1) == 1
+    assert t2([3, 0, 4, 0, 99], 100) == 100
+
+
+# def test_param():
+#     e = TuringMachine(state=[1, 0, 0, 0, 99])
+#     assert e.parameters_current_instruction() == [0, 0, 0]
+#
+#     e2 = TuringMachine(state=[1, 1, 2, 3, 99])
+#     assert e2.parameters_current_instruction() == [3, 2, 1]
+
+
+
+
+if __name__ == '__main__':
+    pass
